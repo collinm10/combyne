7087886e4a1b8b03a39c9ec42e79e2e7c683fd05
collinm10 <crmontag@ncsu.edu>
2023-04-10 18:49:56 -0400

    Almost done

--- src/Components/WinSection.js
+++ src/Components/WinSection.js
@@ -12,69 +12,68 @@ function WinSection() {
                 What do the winners get?
             </div>
             <div className = "WinSection-week-grid">
-                <div className="week-header w1">
-                    Round 1
+                <div className="one-column">
+                    <div className="week-header w1">
+                        Round 1
+                    </div>
+                    <hr className="hr-line"/>
+                    <div className="reward-item w1">
+                        Nothing.
+                    </div>
                 </div>
-                <div className="week-header w2">
-                    Round 2
+                
+                <div className="one-column">
+                    <div className="week-header w2">
+                        Round 2
+                    </div>
+                    <hr className="hr-line"/>
+                    <div className="reward-item w2">
+                        Still nothing.
+                    </div>
                 </div>
-                <div className="week-header w3">
-                    Round 3
-                </div>
-                <div className="week-header w4">
-                    Round 4
-                </div>
-                <div className="week-header w5">
-                    Championship
-                </div>
-
-                <hr className="hr-line"/>
-                <hr className="hr-line"/>
-                <hr className="hr-line"/>
-                <hr className="hr-line"/>
-                <hr className="hr-line"/>
-
-                <div className="reward-item w1">
-                    Nothing.
-                </div>
-                <div className="reward-item w2">
-                    Still nothing.
-                </div>
-                <div className="reward-item w3">
-                    30,000 XP
-                </div>
-                <div className="reward-item w4">
-                    40 Radianite
-                </div>
-                <div className="reward-item w5">
-                    40 Radianite
+                
+                <div className="one-column">
+                    <div className="week-header w3">
+                        Round 3
+                    </div>
+                    <hr className="hr-line"/>
+                    <div className="reward-item w3">
+                        30,000 XP
+                    </div>
+                    <div className="reward-item w3">
+                        Bragging rights.
+                    </div>
                 </div>
 
-                <div>
+                <div className="one-column">
+                    <div className="week-header w4">
+                        Round 4
+                    </div>
+                    <hr className="hr-line"/>
+                    <div className="reward-item w4">
+                        40 Radianite
+                    </div>
+                    <div className="reward-item w4">
+                        Make fun of all the Round 3 losers.
+                    </div>
                 </div>
 
-                <div className="">
+                <div className="one-column">
+                    <div className="week-header w5">
+                        Championship
+                    </div>
+                    <hr className="hr-line"/>
+                    <div className="reward-item w5">
+                        40 Radianite
+                    </div>
+                    <div className="reward-item w5">
+                        500 VALORANT Points
+                    </div>
+                    <div className="reward-item w5">
+                        Best fanbase in the world!
+                    </div>
                 </div>
-
-                <div className="reward-item w3">
-                    Bragging rights.
-                </div>
-
-                <div className="reward-item w4">
-                    Make fun of all the Round 3 losers.
-                </div>
-
-                <div className="reward-item w5">
-                    500 VALORANT Points
-                </div>
-
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div className="reward-item w5">
-                    Best fanbase in the world!
-                </div>
+                
             </div>
         </div>
     );
