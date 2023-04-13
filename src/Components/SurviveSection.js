cbae29c03a7c849d98c70fa29736f770ebb1d7a9
collinm10 <crmontag@ncsu.edu>
2023-04-13 14:47:22 -0400

    Updated grammer

--- src/Components/SurviveSection.js
+++ src/Components/SurviveSection.js
@@ -11,7 +11,7 @@ function SurviveSection() {
                 Survive.
             </div>
             <div className="SurviverSubtext d-flex flex-column align-items-center">
-                The tournament is single elimination. Each round will last a week ending on mondays at 10 AM EST.
+                The tournament is single elimination. Each round will last a week ending on Mondays at 10 AM EST.
             </div>
             <div className="acbracket-wrapper">
                 <img src={ BracketPic } />
